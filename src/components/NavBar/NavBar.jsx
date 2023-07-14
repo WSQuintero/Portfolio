143cce23dfb8105812d68936964a7ede82e721bc
santiagoQ <santiagoquinteroperfil@gmail.com>
2023-07-14 18:14:55 -0500

    corregido conflicto con origin

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,10 +5,34 @@ import { GiHamburgerMenu } from 'react-icons/gi'
 import { IconContext } from 'react-icons'
 
 function NavBar () {
+  const navigate = useNavigate()
+  const { movilSize, listeningResize } = useContext(MyContext)
+
+  listeningResize()
+
   return (
     <>
-    <img src="/navLogo_Mesa de trabajo 1_Mesa de trabajo 1.svg" alt="" className='w-[20%]' />
-      <nav></nav>
+      {!movilSize
+        ? (
+        <>
+          <img
+            src='/navLogo_Mesa de trabajo 1_Mesa de trabajo 1.svg '
+            alt=''
+            className='w-[20%] cursor-pointer '
+            onClick={() => navigate('/')}
+          />
+          <nav className='flex gap-3 text-xl '>
+            <NavLink to={'/about-me'}>About me</NavLink>
+            <NavLink to={'/projects'}>Projects</NavLink>
+            <NavLink to={'/contact'}>Contact me</NavLink>
+          </nav>
+        </>
+          )
+        : (
+        <IconContext.Provider value={{ className: 'w-[40px] h-[40px] text-gray-200 ml-10' }}>
+          <GiHamburgerMenu />
+        </IconContext.Provider>
+          )}
     </>
   )
 }
