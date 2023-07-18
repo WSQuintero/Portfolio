986be518126ef9faca4dfd77419fc3421d823db1
santiagoQ <santiagoquinteroperfil@gmail.com>
2023-07-18 18:27:48 -0500

    estableciendo estilos, imagenes y texto de about-me

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,8 +1,125 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { IconContext } from 'react-icons'
+import { BsArrowRightCircleFill } from 'react-icons/bs'
 
 function AboutMe () {
+  const scroll = useRef(null)
+  const [isScrolled, setIsScrolled] = useState(false)
+  const imgLogos = [
+    '/html.svg',
+    '/css.svg',
+    '/js (2).svg',
+    '/React.svg',
+    '/tailwind.svg',
+    '/git 2.svg'
+  ]
+
+  const handleScroll = () => {
+    const containerScrollWidth = scroll.current.clientWidth
+    if (!isScrolled) {
+      scroll.current.scrollLeft += containerScrollWidth
+      setIsScrolled(true)
+    } else {
+      scroll.current.scrollLeft = 0
+      setIsScrolled(false)
+    }
+  }
+
   return (
-    <div>AboutMe</div>
+    <div className='h-full w-full flex flex-col lg:flex-row'>
+      <section className='w-full lg:w-2/4 bg-[#CECECE] h-[100vh] flex justify-center items-center relative'>
+        <figure className='flex flex-col items-center justify-center w-full'>
+          <img
+            src='https://i.ibb.co/VgBY45s/Rostro-01.png'
+            className='w-[400px] '
+            alt=''
+          />
+          <img
+            src='/Logo_Mesa de trabajo_color.svg'
+            className=' absolute w-[300px] h-[300px] bottom-0 '
+            alt=''
+          />
+        </figure>
+        <figure>
+          <img src='/Logo_Mesa de trabajo_color.svg' alt='' />
+        </figure>
+      </section>
+      <section className='w-full lg:w-1/5 bg-[#5B5B5B] h-[200px] lg:h-[100vh] '></section>
+      <section className='w-full lg:w-4/6 bg-[#CECECE] h-[100vh] flex flex-col justify-center items-center p-20 relative gap-10'>
+        <article className='border border-[#5B5B5B] p-5'>
+          <h1 className='text-6xl text-blue-400 w-full text-start'>Sobre mi</h1>
+          <p className='mt-10 text-justify'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            asperiores pariatur quam hic sapiente tempore consequuntur, odit a?
+            Animi ex praesentium eum laborum, consectetur quam? Tenetur magnam
+            nobis, sit repellat cum a quasi adipisci dolores eius ipsam
+            assumenda vero eos dignissimos dolorem facilis omnis consequuntur
+            laboriosam minus ratione libero eum.
+          </p>
+        </article>
+        <article className='border border-[#5B5B5B] p-5 w-full'>
+          <h2 className=' text-6xl text-blue-400 w-[99%] text-start flex justify-between items-center relative  '>
+            Estudios
+            <button
+              onClick={handleScroll}
+              className={`${
+                isScrolled ? 'rotate-180' : 'rotate-0'
+              } transition-rotate duration-500 `}
+            >
+              <IconContext.Provider
+                value={{
+                  className:
+                    'text-gray-500 w-[40px] h-[40px] cursor-pointer hover:text-red-400 transition duration-[200ms] ease-in-out hover:scale-125 '
+                }}
+              >
+                <BsArrowRightCircleFill />
+              </IconContext.Provider>
+            </button>
+          </h2>
+          <div
+            className='flex overflow-x-hidden sm:overflow-y-hidden w-full items-center scroll-smooth  sm:h-[60px] gap-3 mt-5  '
+            ref={scroll}
+          >
+            <div className='w-[100vw] flex items-center bg-[#A3A3A3]  '>
+              <div className='w-full shrink-0 text-justify'>
+                <p className='p-5'>
+                  {' '}
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Illum nesciunt delectus illo quaerat voluptates atque autem
+                  vero quia, odio in? Dicta enim ipsam consequatur a laboriosam,
+                  corporis quaerat vitae dolorem.
+                </p>
+              </div>
+              <div className=' w-full shrink-0 p-10'>
+                <ol className='flex justify-between items-center'>
+                  {imgLogos.map((logo, index) => (
+                    <li key={index}>
+                      <img
+                        src={logo}
+                        alt=''
+                        className='w-[80px] h-[80px] shrink-0'
+                      />
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+          </div>
+        </article>
+        <article className='border border-[#5B5B5B] p-5 w-full'>
+          <h3 className='mt-10 text-6xl text-blue-400 w-full text-start'>
+            Aspiraciones
+          </h3>
+          <p className='mt-10'>
+            {' '}
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum
+            nesciunt delectus illo quaerat voluptates atque autem vero quia,
+            odio in? Dicta enim ipsam consequatur a laboriosam, corporis quaerat
+            vitae dolorem.
+          </p>
+        </article>
+      </section>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,13 @@ import { useNavigate } from 'react-router'
 import { MyContext } from '../../context/MyContext/MyContext'
 
 function Home () {
-  const [changeLogo, setChangeLogo] = useState(false)
   const navigate = useNavigate()
+  const [changeLogo, setChangeLogo] = useState(false)
   const { movilSize } = useContext(MyContext)
 
   return (
     <div className='flex flex-col sm:flex-row justify-center items-center h-[calc(100vh)] w-full z-0'>
-      <div className='w-full sm:w-[50%] bg-[#CECECE] h-full flex justify-center items-center  relative  py-[50px] '>
+      <section className='w-full sm:w-[50%] bg-[#CECECE] h-full flex justify-center items-center  relative  py-[50px] '>
         {!movilSize && (
           <figure
             className=' left-8  flex justify-center items-center absolute top-[40px] z-20 '
@@ -30,8 +30,8 @@ function Home () {
             />
           </figure>
         )}
-      </div>
-      <div className='w-full sm:w-[50%] bg-[#5B5B5B] h-full flex flex-col justify-center items-center sm:justify-end sm:items-end sm:pr-20 pb-20 gap-3 z-0'>
+      </section>
+      <section className='w-full sm:w-[50%] bg-[#5B5B5B] h-full flex flex-col justify-center items-center sm:justify-end sm:items-end sm:pr-20 pb-20 gap-3 z-0'>
         <h1 className='flex items-center text-center w-[250px] sm:w-[334px] sm:text-end text-6xl text-blue-400 z-20 min-w-[200px] lg:w-full lg:justify-end '>
           Desarrollador Front end
         </h1>
@@ -52,13 +52,13 @@ function Home () {
             About me
           </button>
         </div>
-      </div>
+      </section>
       <figure className=' max-w-[400px] max-h-[400px] absolute overflow-hidden flex justify-center items-center top-40 sm:top-auto  z-0'>
         <img
           src='https://i.ibb.co/tJgCrCP/rostro-editado-1-1.png'
           alt='rostro-editado-1-1'
           border='0'
-          className=' w-[300px] h-[300px] object-cover '
+          className=' w-[400px] h-[400px] object-cover '
         />
       </figure>
     </div>
