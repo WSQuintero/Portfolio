076b9de088a08bf0b7c59d040d7867e010fadf9c
santiagoQ <santiagoquinteroperfil@gmail.com>
2023-07-22 14:05:36 -0500

    estableciendo hover correcto a projectos

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,20 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { projects } from '../../DB/projects'
 
 function Projects () {
+  const [hoverState, setHoverState] = useState({})
+
+  const handleMouseEnter = (project) => {
+    setHoverState((prevState) => ({
+      ...prevState,
+      [project]: true
+    }))
+  }
+
+  const handleMouseLeave = (project) => {
+    setHoverState((prevState) => ({
+      ...prevState,
+      [project]: false
+    }))
+  }
+
   return (
     <div className='h-full flex flex-col sm:flex-row'>
-      <aside className='w-full sm:w-1/4 bg-[#5B5B5B] h-[200px] sm:min-h-[100vh] mt-[60px] sm:mt-0'></aside>
+      <aside className='w-full sm:w-1/4 bg-[#5B5B5B] h-[200px] sm:min-h-[100vh] mt-[60px] sm:mt-0 flex justify-center items-center'>
+        <h1 className='sm:-rotate-90 text-9xl text-[#CECECE]'>Projectos</h1>
+      </aside>
       <section
-        className={'w-full sm:w-3/4 bg-[#CECECE] min-h-[100vh] flex flex-col sm:grid  sm:grid-cols-2 sm:pt-[60px]'}
+        className={
+          'w-full sm:w-3/4 bg-[#CECECE] min-h-[100vh] flex flex-col sm:grid  sm:grid-cols-2 sm:pt-[60px]'
+        }
       >
         {projects.map((project) => (
           <article
             key={project.project}
-            className='bg-gray-600 w-full h-[300px] sm:w-[1fr] sm:h-full hover:bg-[#CECECE]'
+            className='bg-gray-600 w-full h-[300px] sm:w-[1fr] sm:h-full relative'
+            onMouseEnter={() => handleMouseEnter(project.project)} // Usar la función con el nombre del proyecto
+            onMouseLeave={() => handleMouseLeave(project.project)}
           >
-            <img src={project.img} alt='' />
-            <p>{project.project}</p>
+            <div className="z-0">
+              <img src={project.img} alt='' />
+              <p>{project.project}</p>
+            </div>
+            {hoverState[project.project] && (
+              <div className='bg-gray-200 w-full h-[300px] sm:w-[1fr] sm:h-full absolute top-0 z-10'>
+              </div>
+            )}
           </article>
         ))}
       </section>
